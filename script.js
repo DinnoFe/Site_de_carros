1cc210a59fd7f750fc95ddc25436053dec10d5b6
Felipe França <0double.xp0@gmail.com>
2024-08-09 03:19:03 -0300

    Correcao de bug no carrosel
    bug na rotacao de items no carrosel

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ nextButton.onclick = () => {
 }
 
 prevButton.onclick = () => {
-    console.log('Botao prev')
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    items[active].classList.add('active')
 }
 
